0a998dfc022a45786e0073f8fe0ce6e1a5976690
Konstantin Koscheev <stud115857@vyatsu.ru>
2022-02-19 16:11:29 +0300

    Лаб. 2

--- app/src/main/java/com/example/voiceassistent/AI.java
+++ app/src/main/java/com/example/voiceassistent/AI.java
@@ -3,9 +3,11 @@ package com.example.voiceassistent;
 import android.graphics.Path;
 import android.graphics.RadialGradient;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -19,7 +21,6 @@ import java.util.stream.Collectors;
 
 public class AI {
     final static Map<String, String> map = new HashMap<>();
-    public static String quest;
 
     static {
         map.put("(?i)[a-я\\s]*Привет[а-я\\s]*", "Привет");
@@ -36,7 +37,7 @@ public class AI {
 
         map.put("(?i)([а-я\\s]*Что ты умеешь[\\s\\?]*)", "Я умею отвечать на вопросы");
         map.put("(?i)([а-я\\s]*What can you do[\\s\\?]*)", "I know how to answer questions");
-
+/*
         map.put("(?i)([а-я\\s]*Какой сегодня день[\\s\\?]*)", Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + "");
         map.put("(?i)([а-я\\s]*What today[\\s\\?]*)", Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + "");
 
@@ -47,10 +48,12 @@ public class AI {
         map.put("(?i)([а-я\\s]*What day of the week[\\s\\?]*)", getDayOfWeek(LocalDateTime.now(ZoneId.of("Europe/Moscow")).getDayOfWeek().toString()));
 
         map.put("(?i)([а-я\\s]*Сколько дней до (\\d|[0-2]\\d|3[0-1])\\.(\\d|0\\d|1[0-2])\\.(19\\d\\d|2[\\d]{3})[\\s\\?]*)", quest + "");
-        map.put("(?i)([а-я\\s]*How many days before (\\d|[0-2]\\d|3[0-1])\\.(\\d|0\\d|1[0-2])\\.(19\\d\\d|2[\\d]{3})[\\s\\?]*)", quest + " <--");
+        map.put("(?i)([а-я\\s]*How many days before (\\d|[0-2]\\d|3[0-1])\\.(\\d|0\\d|1[0-2])\\.(19\\d\\d|2[\\d]{3})[\\s\\?]*)", quest + " <--");*/
     }
 
-    private static String getDayOfWeek(String day) {
+    private static String getDayOfWeek() {
+        String day = LocalDateTime.now(ZoneId.of("Europe/Moscow")).getDayOfWeek().toString();
+
         if ("SATURDAY".equals(day)) return "Суббота";
         if ("SUNDAY".equals(day)) return "Воскресенье";
         if ("MONDAY".equals(day)) return "Понедельник";
@@ -58,22 +61,54 @@ public class AI {
         if ("WEDNESDAY".equals(day)) return "Среда";
         if ("THURSDAY".equals(day)) return "Четверг";
         if ("FRIDAY".equals(day)) return "Пятница";
+
         return "Думаю...";
     }
 
-    private static String getDifferenceBetweenDates() {
-        System.out.println(quest);
-        return "2";
+    private static String getDifferenceBetweenDates(String question) {
+        String[] d;
+
+        for (String s : question.split("\\s")) {
+            d = s.split("\\.");
+            if (d.length == 3) {
+                LocalDateTime now = LocalDateTime.now(ZoneId.of("Europe/Moscow"));
+                LocalDateTime date = LocalDateTime.of(Integer.parseInt(d[2]),
+                        Integer.parseInt(d[1]), Integer.parseInt(d[0]), 0, 0);
+                long diff = ChronoUnit.DAYS.between(now, date);
+                if (diff < 0)
+                    return "Дата уже прошла";
+                if (diff == 0)
+                    return "До данной даты осталось меньше дня";
+                if (diff > 1)
+                    return "Дней до данной даты: " + diff;
+            }
+        }
+
+        return "...";
     }
 
     protected static String getAnswer(String question) {
-        quest = question;
-
         Optional<String> answer = map.entrySet()
                 .stream()
                 .filter(entry -> Pattern.matches(entry.getKey(), question))
                 .map(Map.Entry::getValue).findFirst();
 
-        return (answer.isPresent()) ? answer.get() : "...";
+        if (answer.isPresent())
+            return answer.get();
+
+        if (Pattern.matches("(?i)([а-я\\s]*Какой сегодня день[\\s\\?]*)", question))
+            return Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + "";
+
+        if (Pattern.matches("(?i)([а-я\\s]*Который час[\\s\\?]*)", question))
+            return LocalDateTime.now(ZoneId.of("Europe/Moscow")).getHour() + "";
+
+        if (Pattern.matches("(?i)([а-я\\s]*Какой день недели[\\s\\?]*)", question))
+            return getDayOfWeek();
+
+        if (Pattern.matches("(?i)([а-я\\s]*How many " +
+                "(\\d|[0-2]\\d|3[0-1])\\.(\\d|0\\d|1[0-2])\\.(19\\d\\d|2[\\d]{3})[\\s\\?]*)", question))
+            return getDifferenceBetweenDates(question) + "";
+
+        return "...";
     }
 }
